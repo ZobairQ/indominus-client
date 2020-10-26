339bd58ae8612bf296abda47f85d5c228534356f
ZobairQ <ZobairQ@users.noreply.github.com>
2020-10-26 21:06:17 +0100

    Sass 7-in-1 architecture added (#2)
    * CSS Changes
    - New header using flexbox
    - Citypage under construction
    
    * More styling added to the city page
    
    * 7-in-1 implemented
    
    Co-authored-by: ZobairQ <zobaiq55@gmail.com>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,26 @@
 import React, { Component } from "react";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import './header.styles.scss';
+import "./header.styles.scss";
 
 export class Header extends Component {
   render() {
     return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography variant="h5">
-            <Link to="/home"><div className="headerName">Home</div></Link>
-          </Typography>
-          <Typography variant="h5">
-            <Link to="/city"><div className="headerName">City</div></Link>
-          </Typography>
-          <Typography variant="h5">
-            <Link to="/attack"><div className="headerName">Attack</div></Link>
-          </Typography>
-          <Typography variant="h5">
-            <Link to="/login"><div className="headerName">Login</div></Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <ul className="navigation">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/city">City</Link>
+          </li>
+          <li>
+            <Link to="/attack">Attack</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
